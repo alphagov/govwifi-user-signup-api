ea62a4300432e6be09c17dc5561c87eaa5591a8c
Jakub Naliwajek <jakub@madetech.com>
2018-07-31 11:09:50 +0100

    Add verbose logging to verify payloads sent to the Performance Platform

--- lib/performance_platform/use_case/send_performance_report.rb
+++ lib/performance_platform/use_case/send_performance_report.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 class PerformancePlatform::UseCase::SendPerformanceReport
   def initialize(stats_gateway:, performance_gateway:)
     @stats_gateway = stats_gateway
@@ -7,6 +9,8 @@ class PerformancePlatform::UseCase::SendPerformanceReport
   def execute(presenter:)
     stats = stats_gateway.fetch_stats
     performance_data = presenter.present(stats: stats)
+    logger = Logger.new(STDOUT)
+    logger.info("Sending performance data: #{performance_data}")
 
     performance_gateway.send_stats(performance_data)
   end
