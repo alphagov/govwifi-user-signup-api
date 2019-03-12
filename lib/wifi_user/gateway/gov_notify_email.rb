21382c9a9d12363ac7edc212ff81863fc8c52b71
Anthony King <anthonydking@gmail.com>
2019-03-12 17:12:49 +0000

    add the gov notify email gateway (#168)
    * add the gov notify email gateway
    
    * optionally allow including a 'reply to id' for emails
    
    * reduce length of subject line

---
+++ lib/wifi_user/gateway/gov_notify_email.rb
@@ -0,0 +1,32 @@
+require 'notifications/client'
+
+class WifiUser::Gateway::GovNotifyEmail
+  def initialize(api_key)
+    @client = Notifications::Client.new(api_key)
+  end
+
+  def execute(email_address:, template_id:, template_parameters: {}, reply_to_id: nil)
+    begin
+      client.send_email(
+        email_address: email_address,
+        template_id: template_id,
+        personalisation: template_parameters,
+        email_reply_to_id: reply_to_id
+      )
+      success = true
+    rescue Notifications::Client::RequestError => e
+      raise if !is_validation_error?(e)
+
+      success = false
+    end
+    WifiUser::Domain::EmailResponse.new(success: success)
+  end
+
+private
+
+  attr_accessor :client
+
+  def is_validation_error?(error)
+    error.message.dig(0, 'error') == 'ValidationError'
+  end
+end
