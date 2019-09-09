alphagov/govwifi-user-signup-api

commit b2033d87009c7b1e0c95448a0e248bbf3b57475e

Add task to generate volumetrics by month

tasks/publish_statistics.rb
@@ -14,6 +14,20 @@ task :publish_daily_statistics, :date do |_, args|
   ).execute(presenter: volumetrics_presenter)
 end
 
+task :publish_monthly_statistics, :date do |_, args|
+  args.with_defaults(date: Date.today.to_s)
+  logger.info("publishing monthly statistics with #{args}")
+  performance_gateway = PerformancePlatform::Gateway::PerformanceReport.new
+  volumetrics_gateway = PerformancePlatform::Gateway::Volumetrics.new(date: args[:date])
+  volumetrics_presenter = PerformancePlatform::Presenter::Volumetrics.new(date: args[:date], period: 'month')
+
+  PerformancePlatform::UseCase::SendPerformanceReport.new(
+    stats_gateway: volumetrics_gateway,
+    performance_gateway: performance_gateway
+  ).execute(presenter: volumetrics_presenter)
+end
+
+
 task :publish_weekly_statistics, :date do |_, args|
   args.with_defaults(date: Date.today.to_s)
   logger.info("publishing weekly statistics #{args}")

commit 17e01a5bf18d13aa04ea4240bf70b729af305573

Fix publish_monthly_statistics task by passing the right arguments
- In #220 we added a new task to send monthly volumetrics to the
  Performance Platform
- publish_monthly_statistics has been throwing an ArgumentError because we
  were passing period to PerformancePlatform::Presenter::Volumetrics.
  This was a mistake introduced in b2033d87009c7b1e0c95448a0e248bbf3b57475e
  PerformancePlatform::Gateway::Volumetrics is the
  one that was extended on 089680e016ab199dfa86e3773892db8b55ffa74e
  to support 'day' and 'month' periods.

tasks/publish_statistics.rb
@@ -18,8 +18,8 @@ task :publish_monthly_statistics, :date do |_, args|
   args.with_defaults(date: Date.today.to_s)
   logger.info("publishing monthly statistics with #{args}")
   performance_gateway = PerformancePlatform::Gateway::PerformanceReport.new
-  volumetrics_gateway = PerformancePlatform::Gateway::Volumetrics.new(date: args[:date])
-  volumetrics_presenter = PerformancePlatform::Presenter::Volumetrics.new(date: args[:date], period: 'month')
+  volumetrics_gateway = PerformancePlatform::Gateway::Volumetrics.new(date: args[:date], period: 'month')
+  volumetrics_presenter = PerformancePlatform::Presenter::Volumetrics.new(date: args[:date])
 
   PerformancePlatform::UseCase::SendPerformanceReport.new(
     stats_gateway: volumetrics_gateway,