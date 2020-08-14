11ccc860aa75936c40c8cc010ea0110d82162527
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-08-14 14:14:22 +0100

    Add Notify gateway to retrieve information about notifications.
    The Notify API supplies the notifications in batches of 250. The
    gateway implementation returns an enum that automatically retrieves
    the next batch when necessary.

---
+++ lib/notifications/gateway/notify.rb
@@ -0,0 +1,21 @@
+require "notifications/client"
+
+module Notifications
+  module Gateway
+    class Notify
+      def initialize(api_key)
+        @client = Notifications::Client.new(api_key)
+      end
+
+      def to_enum
+        args = {}
+        Enumerator.new do |yielder|
+          until (notifications = @client.get_notifications(args).collection).empty?
+            notifications.each { |notification| yielder << notification }
+            args.merge!(older_than: notifications.last.id)
+          end
+        end
+      end
+    end
+  end
+end
