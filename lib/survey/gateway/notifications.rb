2c6db640a719b8158f4eedfc9f7d8a209a16cdf3
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2020-11-10 11:34:22 +0000

    fix: send SMS for mobile users
    Co-authored-by: Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>

--- lib/survey/gateway/notifications.rb
+++ lib/survey/gateway/notifications.rb
@@ -3,26 +3,36 @@ require "notifications/client"
 class Survey::Gateway::Notifications
   def initialize(user_model)
     @user_model = user_model
+    @client = Notifications::Client.new(ENV.fetch("NOTIFY_API_KEY"))
   end
 
   def execute
-    send_survey(@user_model.contact)
+    send_survey
   end
 
-  private
+private
 
-  def send_survey(email_address)
-    client = Notifications::Client.new(ENV.fetch("NOTIFY_API_KEY"))
+  def send_survey
+    if is_mobile?
+      send_text
+    else
+      send_email
+    end
+  end
 
-    client.send_email(
-      email_address: email_address,
-      # template_id: credentials_template_id,
-      # personalisation: @user_model.generate(contact: email_address),
-      # email_reply_to_id: do_not_reply_email_address_id,
+  def send_email
+    @client.send_email(
+      email_address: @user_model.contact,
     )
   end
 
-  def credentials_template_id
-    YAML.load_file("config/#{ENV['RACK_ENV']}.yml").fetch("notify_email_template_ids").fetch("self_signup_credentials")
+  def send_text
+    @client.send_sms(
+      phone_number: @user_model.contact,
+    )
+  end
+
+  def is_mobile?
+    @user_model.contact.start_with? "+"
   end
 end
--- spec/lib/survey/gateway/notifications_spec.rb
+++ spec/lib/survey/gateway/notifications_spec.rb
@@ -1,30 +1,59 @@
-describe Survey::Gateway::Notifications, :focus do
-  let!(:user) { FactoryBot.create(:user_details) }
+describe Survey::Gateway::Notifications do
+  let(:user) { FactoryBot.create(:user_details) }
+  let(:mobile_user) { FactoryBot.create(:user_details, :sms) }
 
   let(:notify_email_url) { "https://api.notifications.service.gov.uk/v2/notifications/email" }
+  let(:notify_mobile_url) { "https://api.notifications.service.gov.uk/v2/notifications/sms" }
+
   let(:notify_email_request) do
     {
       email_address: user.contact,
     }
   end
 
-  let(:notify_email_stub) do
-    stub_request(:post, notify_email_url)
-      .with(body: notify_email_request)
+  let(:notify_mobile_request) do
+    {
+      phone_number: mobile_user.contact,
+    }
+  end
+
+  let(:notify_mobile_stub) do
+    stub_request(:post, notify_mobile_url)
+      .with(body: notify_mobile_request)
       .to_return(status: 200, body: {}.to_json)
   end
 
-  before do
-    notify_email_stub
-  end
-
   context "when the user signed up via email" do
+    let(:notify_email_stub) do
+      stub_request(:post, notify_email_url)
+        .with(body: notify_email_request)
+        .to_return(status: 200, body: {}.to_json)
+    end
+
+    before do
+      notify_email_stub
+    end
+
     let(:gateway) { Survey::Gateway::Notifications.new(user) }
 
     it "sends an email to them" do
       gateway.execute
 
       expect(notify_email_stub).to have_been_requested.times(1)
+    end
+  end
+
+  context "when the user signed up via mobile" do
+    let(:gateway) { Survey::Gateway::Notifications.new(mobile_user) }
+
+    before do
+      notify_mobile_stub
+    end
+
+    it "sends a text to them" do
+      gateway.execute
+
+      expect(notify_mobile_stub).to have_been_requested.once
     end
   end
 end
